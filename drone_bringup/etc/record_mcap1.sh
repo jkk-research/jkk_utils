1a301ea8cff2146a76f9b5afd63efded2c4ff750
horverno <horverno@gmail.com>
2023-11-17 13:20:57 +0100

    Mcap cache-size and ouster disable image

--- drone_bringup/etc/record_mcap1.sh
+++ drone_bringup/etc/record_mcap1.sh
@@ -2,15 +2,16 @@
 
 ## ROS 2 rosbag recorder (MCAP format)
 
-cd /bag # TODO: check if correct
+cd /bag
 mkdir -p $(date -I)
 cd $(date -I)
 TEXT1="$1"
 TIME1="$(date +"%Y-%m-%d_%H-%M")"
-FILE1="$TEXT1$TIME1"
+FILE1="$TEXT1_drone1_$TIME1"
 FILE2="x_rosparam_dump_$TEXT1$TIME1.txt"
 PWD1="$(pwd)"
 echo "Writing to file: $PWD1/$FILE1"
-ros2 bag record -s mcap --all -o $FILE1 
+# 524288000 byte is ~0.5 GB
+ros2 bag record -s mcap --all -o $FILE1 --max-cache-size 524288000
 
 
