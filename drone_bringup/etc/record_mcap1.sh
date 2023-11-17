df7e9933a2d45ef3b316978290678d8f348b1952
horverno <horverno@gmail.com>
2023-11-17 14:32:59 +0100

    Udpated cache-size

--- drone_bringup/etc/record_mcap1.sh
+++ drone_bringup/etc/record_mcap1.sh
@@ -6,12 +6,14 @@ cd /bag
 mkdir -p $(date -I)
 cd $(date -I)
 TEXT1="$1"
+VEH1="_drone1_"
 TIME1="$(date +"%Y-%m-%d_%H-%M")"
-FILE1="$TEXT1_drone1_$TIME1"
+FILE1="$TEXT1$VEH1$TIME1"
 FILE2="x_rosparam_dump_$TEXT1$TIME1.txt"
 PWD1="$(pwd)"
 echo "Writing to file: $PWD1/$FILE1"
 # 524288000 byte is ~0.5 GB
-ros2 bag record -s mcap --all -o $FILE1 --max-cache-size 524288000
+# 1048576000 byte is ~ 1 GB
+ros2 bag record -s mcap --all -o $FILE1 --max-cache-size 1048576000 --storage-config-file mcap_writer_options1.yaml
 
 
