76fed3874aee9984e2224d5aee7e5349c19a52fc
Patrik Bezi <p4t0r1k@yahoo.com>
2023-11-17 13:23:00 +0100

    launch file updated

--- time_utils/launch/display_time.launch.py
+++ time_utils/launch/display_time.launch.py
@@ -1,17 +1,23 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    PythonExpression,
+)
 
 def generate_launch_description():
     return LaunchDescription([
-        ## TODO: ros2 launch time_utils display_time.launch.py topic:=other_topic
         DeclareLaunchArgument("topic", description="a topic to process"),
         Node(
             package='time_utils',
             executable='display_time',
             output='screen',
             parameters=[
-                {'topic': '/drone1/gps/duro/time_ref'}
+                {
+                    'topic': LaunchConfiguration("topic")
+                }
             ]
         )
     ])
