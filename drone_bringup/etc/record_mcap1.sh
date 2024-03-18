d79832c5186ed92cbb0b5690446ee66a53464b1c
umiklos <ungermiklos1992@gmail.com>
2024-03-18 15:29:10 +0100

    add rs_lidar poincloud to record_mcap

--- drone_bringup/etc/record_mcap1.sh
+++ drone_bringup/etc/record_mcap1.sh
@@ -14,7 +14,7 @@ PWD1="$(pwd)"
 echo "Writing to file: $PWD1/$FILE1"
 # 524288000 byte is ~0.5 GB
 # 1048576000 byte is ~ 1 GB
-ros2 bag record -s mcap -o $FILE1 --max-cache-size 1048576000 --storage-config-file $DIR1/mcap_writer_options1.yaml /drone1/gps/duro/current_pose /drone1/gps/duro/imu /drone1/gps/duro/mag /drone1/gps/duro/navsatfix /drone1/gps/duro/status_flag /drone1/gps/duro/status_string /drone1/gps/duro/time_ref /drone1/os/points /tf /tf_static
+ros2 bag record -s mcap -o $FILE1 --max-cache-size 1048576000 --storage-config-file $DIR1/mcap_writer_options1.yaml /drone1/gps/duro/current_pose /drone1/gps/duro/imu /drone1/gps/duro/mag /drone1/gps/duro/navsatfix /drone1/gps/duro/status_flag /drone1/gps/duro/status_string /drone1/gps/duro/time_ref /drone1/os/points /tf /tf_static /rslidar_points
 
  
 
