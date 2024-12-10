896339eb532b7b1d2e2d4c9cb944c89c4f4e5caf
szepilot <szepilot@gmail.com>
2024-12-10 15:51:09 +0100

    Update lanelet and pcd

--- demo_jkk/launch/lanelet_visualize.launch.py
+++ demo_jkk/launch/lanelet_visualize.launch.py
@@ -3,11 +3,13 @@ from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from math import pi, degrees, radians
 
 def generate_launch_description():
     DeclareLaunchArgument("map", description="a lanelet process", default_value="gyor1"),
     ## possible values: gyor1, gyor2, zala_uni, zala_motorway, zala_city
     return LaunchDescription([
+        ## TODO: choose based on LaunchConfiguration("map")
         Node(
             package='lanelet2_rviz2',
             executable='visualize_osm',
@@ -15,27 +17,28 @@ def generate_launch_description():
             parameters=[
                 {"frame_id": "map_gyor_0"},
                 {"line_width": 1.0},
+                {"osm_filename": "/home/dev/autoware_map/gyor_uni.osm"},
+                {"center_map": False},
+                {"speed_color_max": 90.0},
+            ],
+        ),       
+        Node(
+            package='pcd_publisher',
+            executable='pcd_publisher',
+            output='screen',
+            parameters=[
+                {"pcd_file_path": "/mnt/bag/dlio_map2.pcd"},
+                {"frame_id": "map_gyor_0"},
+                {"topic_name": "map"}, # https://github.com/rsasaki0109/lidar_localization_ros2
+                {"rate": 1},
+                {"x_translation": -24.0},
+                {"y_translation": 77.0},
+                {"z_translation": 0.0},
+                {"x_rotation": 0.0},
+                {"y_rotation": 0.0},
+                {"z_rotation": radians(-177.1)}, # -177.1 deg is -3.09097 rad
             ],
         ),
-        ## TODO: choose based on LaunchConfiguration("map")
-        ExecuteProcess(
-            cmd=[
-                'ros2', 'topic', 'pub', '/osm_filename', 
-                'std_msgs/msg/String', 
-                "{data: '/home/dev/autoware_map/gyor_uni.osm'}"
-            ],
-            output='screen'
-        ),
-        # Node(
-        #     package='pcl_ros',
-        #     executable='pcd_to_pointcloud',
-        #     name='pcd_to_pointcloud',
-        #     parameters=[
-        #         {'file_name': '/home/he/dlio_map2.pcd'},
-        #         {'tf_frame': 'map_gyor_0'},
-        #         {'publishing_period_ms': 1000},
-        #     ],
-        # ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
