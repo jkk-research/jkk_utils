de388fa4cff3c6c10dc88380bef12e96e306140d
szepilot <szepilot@gmail.com>
2025-05-15 10:17:27 +0200

    Add new MCAP recording scripts and presets for enhanced measurement capabilities

--- .bash_aliases
+++ .bash_aliases
@@ -5,6 +5,7 @@ alias r2='source ~/ros2_ws/install/setup.bash && echo -e "  \e[34msource\e[0m \e
 alias aw='source ~/autoware/install/setup.bash && echo -e "  \e[34msource\e[0m \e[33m~/autoware\e[0m/install/setup.bash"'
 alias f1='source ~/f1tenth_ws/install/setup.bash && echo -e "  \e[34msource\e[0m \e[33m~/f1tenth_ws\e[0m/install/setup.bash"'
 alias r1='screen -mdS roscore1 bash -c 'roscore' && echo "screen roscore1"'
+alias crp='. ~/crp_ws/install/setup.bash'
 # wheeltec robot
 alias start_drivers='~/ros2_ws/src/drivers/shell/start_drivers.sh && echo -e " "'
 alias start_lane='~/ros2_ws/src/lane_following_cam/shell/start_lane.sh && echo -e " "'
--- mcap_rec/etc/record_mcap5.sh
+++ mcap_rec/etc/record_mcap5.sh
@@ -1,11 +1,11 @@
-                                        #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 ## ROS 2 rosbag recorder (MCAP format)
 
 DIR1="$(pwd)"
 DIR2=~/ros2_ws/src/jkk_utils/mcap_rec/etc/
 cd /mnt/bag
-DIR3=$(date -I)_bosch # eg. 2025-08-25_bosch
+DIR3=$(date -I) # eg. 2025-08-25
 mkdir -p $DIR3
 cd $DIR3
 VEH1="_lexus3_"
@@ -16,7 +16,7 @@ PWD1="$(pwd)"
 echo "Writing to file: $PWD1/$FILE1"
 # 524288000 byte is ~0.5 GB
 # 1048576000 byte is ~ 1 GB
-TOPICS_FILE=$DIR2/preset_bosch_all.txt
+TOPICS_FILE=$DIR2/preset_base.txt
 TOPICS="$(cat $TOPICS_FILE)"
 ros2 bag record -s mcap -o $FILE1 --max-cache-size 1048576000 --storage-config-file $DIR2/mcap_writer_options1.yaml $TOPICS
 
